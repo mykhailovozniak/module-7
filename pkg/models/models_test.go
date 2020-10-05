5abc024acbd1e1c7429f226d28677e0dfca133fd
Mykhailo <mykhailo_vozniak@epam.com>
2020-10-05 09:52:06 +0300

    fix test

--- pkg/models/models_test.go
+++ pkg/models/models_test.go
@@ -1,1 +1,11 @@
 package models
+
+import "testing"
+
+func TestMaterialModel(t *testing.T)  {
+	material := &Material{Name: "Test material"}
+
+	if material.Name == "" {
+		t.Errorf("Name property should not be empty")
+	}
+}
